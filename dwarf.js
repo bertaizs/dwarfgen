10b3f350755c7e0e36b4809f2a5c4c44b2d56f14
István Zsolt Berta <isti@tuzok.local>
2019-03-14 21:35:15 +0100

    saving

--- dwarf.js
+++ dwarf.js
@@ -1,14 +1,25 @@
+// abstract: danger, knowledge, fame
+
 
 var grammar = {};
 
-grammar['ROOT'] = {
-	'{dwarfname}': true,
-} 
+grammar['/'] = {
+//	'{word}': 100,
+	'{name}': 1000,
+	'{name}, son of {Dwarfname}': 100,
+	'{Dwarfname}, {epithet}': 100,
+}
+
+grammar['name'] = {
+	'{Dwarfname}': 100,
+//	'{Dwarfname}, the {Adjective}': 100,
+	'{Adjective} {First}': 100,	
+	'{Adjective} {Dwarfname}': 100,	
+}
 
 grammar['dwarfname'] = {
-	'{first}, son of {first}': 100,
-	'{first} {surname}': 100,
-	'{first}-{first} {surname}': 10,
+	'{First} {Surname}': 100,
+	'{First}-{First} {Surname}': 5,
 }
 
 grammar['first'] = {
@@ -17,31 +28,87 @@ grammar['first'] = {
 	'Gimli':  	100,
 	'Olaf': 	100,
 	'Hrotgar': 	100,
+	'{blabla}': 10000,
 }
 
 grammar['surname'] = {
-	'{noun}{verb}er': 100,
+	'{Material}{bodypart}': 100,
+	'{Material}{equipment}': 100,
+	'{Material}{verb}er': 100,
+	'{Material}{monster}': 100,
+	'{Adjective}{equipment}': 100,
+	'{Adjective}{bodypart}': 100,
+	'{Equipment}{verb}er': 100,
+	'{Monster}sbane': 100,
+	'{Monster}slayer': 100,
+	'{Monster}hunter': 100,
 }
 
-grammar['noun'] = {
-	'Axe': 100,
-	'Hammer': 100,
-	'Beard': 100,
-/*	'stone': 100,
-	'rock': 100,
-	'gold': 100,
-	'silver': 100,
-	'iron': 100,
-	'steel': 100,
-	'mithril': 1,	// because mithril is a rare metal
-	'thunder': 10, */
+grammar['material'] = {
+	"gold": 100, "silver": 100, "bronze": 100, "mithril": 10, "iron": 100, "steel": 100, "oak": 100, 
+	"rock": 100, "stone": 100, "diamond": 10,
 }
+
+grammar['equipment'] = {
+	"axe": 100, "hammer": 100, "shield": 100, "helm": 100, "mug": 100, "beer": 100, "ale": 100,
+}
+
+grammar['bodypart'] = {
+	"hand": 100, "face": 100, "beard": 500, "leg": 60,
+}
+
+grammar['adjective'] = {
+	"swift": 100, "strong": 100, "fat": 100, "stubborn": 100, "thirsty": 100, "sharp": 100, "blunt": 100, 
+	"brave": 100, "craven": 100, "angry": 100, "hungry": 100, "happy": 100, "sad": 100, "desperate": 100,
+}
+
+grammar['epithet'] = {
+	'the {adjective}': 100,
+	'who makes {monster}s {adjective}': 100,
+	'who makes {monster}s {verb}': 100,
+	'who makes {equipment}s {adjective}': 100,
+	'slayer of {monster}s': 100,
+	'bane of {monster}s': 100,
+	'hunger of {monster}s': 100,
+	'seeker of {monster}s': 100,
+	'the {verb}ing {monster}': 100,
+}
+
+grammar['monster'] = {
+	"monster": 100, "fiend": 10, "demon": 10, "devil": 5,
+	"orc": 100, "troll": 40, "dragon": 5, "slug": 100, "wyvern": 10,
+}
+
 
 grammar['verb'] = {
-	'mine': 100,
-	'crush': 100,
-	'mine': 100,
-	'walk': 100,
+	'crush': 100, 'walk': 100, 'eat': 100, 'speak': 100, 'shout': 100, 'seek': 100, 'fight': 100,
+}
+
+
+grammar['blabla'] = {
+	'{Syllables+}': 100,
+}
+
+grammar['syllables'] = {
+	'': 30,
+	'{syllable}': 200,
+	'{syllable}{syllables}': 30,
+	'{syllable}-{syllables+}': 30,
+}
+
+grammar['syllables+'] = {
+	'{Syllable}{syllables}': 100,
+}
+
+grammar['syllable'] = {
+	'bang': 100, 'bork': 100, 'bronk': 100, 'birk': 100,
+	'dorn': 100, 'dra': 100, 'drak': 100, 'dhung': 100, 'dong': 100,
+	'gond': 100, 'grand': 100,
+	'hork': 100, 'hrog': 100, 'hroth': 100, 'hruk': 100,
+	'nor': 100, 'nard': 100, 'nung': 100,
+	'ong': 100, 'ogh': 100,
+	'tork': 100, 'tra': 100, 'thor': 100, 'than': 100, 'torp': 100, 'trop': 100,
+	'warr': 100, 'wrag': 100, 
 }
 
 
--- namegen.js
+++ namegen.js
@@ -1,6 +1,21 @@
 // name gen
 
-function Rule (param) {
+var namegen = {};
+
+namegen.grammar = { '{/}': {empty: true} }
+	
+// counts keys (attributes) in an object
+namegen.count = function( obj ) { 
+		if( typeof obj == "string")
+			obj = this.grammar[obj];
+		return Object.keys(obj).length; 
+}	
+	
+
+// returns a canonically formatted param as a rule
+// a rule should have a key element and a weight element
+// this makes sure that the weight element is populated; with a default value of 100
+namegen.Rule = function (param) {
 	if( typeof(param) == 'object' ) {
 		if( param.weight == undefined )
 			param.weight = 100;
@@ -19,37 +34,50 @@ function Rule (param) {
 	}
 }
 
-function pickOne( rule ) {
+// receives rules, i.e. an associative array of {key: weight} pairs
+// returns a randomly selected key based on weight
+// e.g. pickone( {foo: 9, bar: 1}) returns "foo" 90% of the time and "bar" 10% of the time
+// if rulename starts with a capital letter, 
+namegen.pickOne = function( rules, rulename ) {
+//	console.log(rule);
 	var probs = {};
 	var max = 0;
 	
-	for( var key in rule ) {
-		var r = new Rule(rule[key]);
+	for( var key in rules ) {
+		var r = this.Rule(rules[key]);
 		probs[key] = max + r.weight;
 		max += r.weight;
 	}
 	
-	if( max == 0 ) throw "no keys in: "+rule;
+	if( max == 0 ) throw "no keys in: "+rules;
 	
 	var rnd = Math.trunc( Math.random()*max );
 	
 	for( var key in probs ) {
-		if( probs[key]>rnd )
-			return key;
+		if( probs[key]>rnd ) {
+			if( rulename[0] == rulename[0].toLowerCase() )
+				return key;		// return transformation as is
+			else
+				return key[0].toUpperCase() + key.slice(1);	// return transformation with first letter capitalized
+		}
 	}	
 	throw "sg went wrong... :(";
 } 
 
-function process( str, grammar ) {
-	var t = str.match( /^(.*?){(.*?)}(.*)$/);
+// returns a random transformation of param str as per param grammar
+// grammar defines a set of rules, str is a pattern to be transformed
+namegen.process = function( str, grammar ) {
+	var t = str.match( /^(.*?){(.*?)}(.*)$/);	// checks if any rules can be appied to str 
 	if( !t )
-		return str;
+		return str;	// if not, returns str as is
 	
-	return t[1] + process( pickOne(grammar[ t[2] ])+ t[3], grammar );
+	// otherwise executes one random transformation, call process on the resulting string and return the result
+	return t[1] + this.process( this.pickOne(grammar[ t[2].toLocaleLowerCase() ], t[2])+ t[3], this.grammar ); 
 }
 
+// default entry point
 function randomName() {
-	return process( '{ROOT}', grammar );
+	return namegen.process( '{/}', grammar );
 }
 
 
