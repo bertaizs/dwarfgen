5e2ba1d001325da49f8ac3afadfc7b4cb298a493
István Zsolt Berta <isti@tuzok.local>
2019-09-08 13:07:02 +0200

    terraform upload now works :)

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,63 @@
 
 variable "google_root" { default = "d2cznr2f" }
-
-variable "ssh_public_key_path" { default = "~/.ssh/id_rsa.pub" }
+#variable "ssh_public_key_path" { default = "~/.ssh/id_rsa.pub" }
 
 variable "domain" { default	= "beltwaan.net" }
 variable "dns_zone_name" { default = "beltwaan" }
+variable "subdomain" { default = "pakmac" }
 
 
 provider "google" {
-	credentials	= "${file("~/.ssh/fergeteg-terraform.json")}"
+	credentials	= "${file("~/.ssh/beltwaan-terraform.json")}"
 	project		= "beltwaan"
 	region		= "us-central1"
 	zone		= "us-central1-c"
 }
 
 
+# register DNS name
 resource "google_dns_record_set" "sub-beltwaan-net" {
-	name = "${var.vm_name}.${var.domain}."
-	type = "A"
+	name = "${var.subdomain}.${var.domain}."
+	type = "CNAME"
 	ttl  = 300
-
 	managed_zone = "${var.dns_zone_name}"
+	rrdatas = ["c.storage.googleapis.com."]
+}
 
-	rrdatas = ["194.38.107.101"]
+# create bucket for DNS name
+resource "google_storage_bucket" "wwwbucket" {
+  name     = "${var.subdomain}.${var.domain}"
+  location = "us-east1"
+  storage_class = "REGIONAL"
+  bucket_policy_only = true
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+}
+
+# make bucket public
+resource "google_storage_bucket_iam_binding" "binding" {
+  bucket = "${var.subdomain}.${var.domain}"
+  role        = "roles/storage.objectViewer"
+  members = [ "allUsers" ]
+}
+
+# upload files -- one by one :(
+resource "google_storage_bucket_object" "index" {
+  name   = "index.html"
+  source = "../www/index.html"
+  bucket = "${var.subdomain}.${var.domain}"
+}
+resource "google_storage_bucket_object" "namegen" {
+  name   = "namegen.js"
+  source = "../www/namegen.js"
+  bucket = "${var.subdomain}.${var.domain}"
+}
+resource "google_storage_bucket_object" "dwarf" {
+  name   = "dwarf.js"
+  source = "../www/dwarf.js"
+  bucket = "${var.subdomain}.${var.domain}"
 }
 
